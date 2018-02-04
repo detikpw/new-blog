e93e548cb76fcddbacfd60d3494f057b07e67c70
Detik <the2ndflamingheart@gmail.com>
2018-02-04 15:17:00 +0700

    Add meta title in blog template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <div>
+      <Helmet title={`${data.site.siteMetadata.title} - ${post.frontmatter.title}`} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
@@ -15,6 +17,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        title
+      }
+    }
+    site {
+      siteMetadata {
         title
       }
     }
