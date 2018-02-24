5f5e9f478338b92aff024a2b22a74b28a964b1f9
Detik <the2ndflamingheart@gmail.com>
2018-02-24 23:14:15 +0700

    feat(link): Add custom style to link

--- src/reusables/link.js
+++ src/reusables/link.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+import { PRIMARY_BLACK } from '../constants/color';
 
-export default ({ to, children, className }) =>
-  <Link className={className} to={to}>{children}</Link>;
+export default styled(({ to, children, className }) =>
+  <Link className={className} to={to}>{children}</Link>)`
+    text-decoration: none;
+    color: ${PRIMARY_BLACK};
+    font-weight: 700;
+  `;
