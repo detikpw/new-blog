7b426f03c346e35f0cd5e0b3ce9610ba1c745bfb
Detik <the2ndflamingheart@gmail.com>
2018-02-24 23:37:05 +0700

    refactor(layout): Change Layout style component with using styled-component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { styled } from 'styletron-react';
+import styled from 'styled-components';
 import { get } from 'lodash/fp';
 import Header from './header';
 import { NAV_LINKS } from '../constants/site-config';
 
-const Layout = styled('div', {
-  display: 'block',
-  borderTopWidth: '5px',
-  borderTopStyle: 'solid',
-  borderTopColor: 'black',
-});
+const Layout = styled.div`
+  display: block;
+  border-top-width: 5px;
+  border-top-style: solid;
+  border-top-color: black;
+`;
 
 export default ({ children, data }) => {
   const siteTitle = get(['site', 'siteMetadata', 'title'])(data);
