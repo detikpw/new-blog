bba1382837fce32f64021c42f6c00bc9ba3949f9
Detik <the2ndflamingheart@gmail.com>
2018-02-24 22:53:41 +0700

    feat(constants): Create site-config constants for temporary

---
+++ src/constants/site-config.js
@@ -0,0 +1,13 @@
+export const NAV_LINKS = [
+  {
+    link: '/categories/opini',
+    label: 'Opini',
+  },
+  {
+    link: 'about',
+    label: 'Tentang Penulis',
+  },
+];
+
+console.log('Temporary caused by rule refer default');
+export default null;
