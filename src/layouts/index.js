3813bd3280d7b11cf08e391fd2834f5d88e72002
Detik <the2ndflamingheart@gmail.com>
2018-02-24 23:28:49 +0700

    feat(layout): Remove PageHeader and about us link from Layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { styled } from 'styletron-react';
-import GatsbyLink from 'gatsby-link';
 import { get } from 'lodash/fp';
-
 import Header from './header';
-import { rhythm } from '../utils/typography';
 import { NAV_LINKS } from '../constants/site-config';
-
-const Link = styled(
-  ({ className, to, children }) =>
-    <GatsbyLink className={className} to={to}>{children}</GatsbyLink>,
-  {
-    float: 'right',
-  },
-);
 
 const Layout = styled('div', {
   display: 'block',
@@ -22,25 +11,11 @@ const Layout = styled('div', {
   borderTopColor: 'black',
 });
 
-const Title = styled('H3', {
-  marginBottom: rhythm(2),
-  display: 'inline-block',
-  fontStyle: 'normal',
-});
-
 export default ({ children, data }) => {
   const siteTitle = get(['site', 'siteMetadata', 'title'])(data);
   return (
     <Layout>
       <Header siteTitle={siteTitle} navLinks={NAV_LINKS} />
-      <GatsbyLink to="/">
-        <Title>
-          {siteTitle}
-        </Title>
-      </GatsbyLink>
-      <Link to="/about/">
-        About
-      </Link>
       {children()}
     </Layout>
   );
