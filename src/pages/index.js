b32d295b84f5762fa7fecb7e124b49df6d62f890
Detik <the2ndflamingheart@gmail.com>
2018-02-24 23:40:11 +0700

    feat(homepage): Remove page title and total post

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,6 @@ import { get } from 'lodash/fp';
 
 import { rhythm } from '../utils/typography';
 
-const Title = styled('h1', {
-  display: 'inline-block',
-  borderBottom: '1px solid',
-});
-
-const Subtitle = styled('h2', null);
-
 const PostTitle = styled('h3', {
   marginBottom: rhythm(1 / 4),
 });
@@ -26,10 +19,6 @@ export default ({ data }) => {
   const markdownRemarks = get('allMarkdownRemark')(data);
   return (
     <div>
-      <Title>
-        Amazing Pandas Eating Things
-      </Title>
-      <Subtitle>{markdownRemarks.totalCount}{' Posts'}</Subtitle>
       {markdownRemarks.edges.map(({ node }) => {
         const frontmatter = get('frontmatter')(node);
         return (
