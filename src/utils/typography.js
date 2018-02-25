1ea163a3adb214a7caf4973a1ed481bc086dd49b
Detik <the2ndflamingheart@gmail.com>
2018-02-25 11:14:44 +0700

    feat(typography): Set margin bottom to 0 for h2

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,11 @@
 import Typography from 'typography';
 import noriegaTheme from 'typography-theme-noriega';
 
+noriegaTheme.overrideThemeStyles = ({ rhythm }) => ({
+  h2: {
+    marginBottom: rhythm(0),
+  },
+});
 
 const typography = new Typography(noriegaTheme);
 
