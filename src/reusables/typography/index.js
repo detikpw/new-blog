25d2b37e8d3e9bf1d7631ca50653c693830d23f5
Detik <the2ndflamingheart@gmail.com>
2018-02-26 02:25:00 +0700

    feat(reusable): Create PageHeader component

--- src/reusables/typography/index.js
+++ src/reusables/typography/index.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const Title = styled.h2``;
+export const PageHeader = Title.extend`
+  && {margin-bottom: 1.3334em;}
+`;
 export const BodyText = styled.p``;
 export const Italic = styled.i``;
