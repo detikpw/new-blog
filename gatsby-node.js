587c1f534a94853141eb38acf27732f8aea941bc
Detik <the2ndflamingheart@gmail.com>
2018-02-26 02:23:47 +0700

    feat(category-template): Set up category page config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const { get, kebabCase, uniq  } = require('lodash/fp');
+
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
@@ -14,13 +16,19 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
 };
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
+  const { createPage } = boundActionCreators;
+
+  const blogPostTemplate = path.resolve('src/templates/blog-post.js');
+  const categoryTemplate = path.resolve('src/templates/category.js');
+  return new Promise((resolve) => {
     graphql(`
       {
         allMarkdownRemark {
           edges {
             node {
+              frontmatter {
+                category
+              }
               fields {
                 slug
               }
@@ -29,18 +37,33 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         }
       }
     `
-).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+).then((result) => {
+      const posts = result.data.allMarkdownRemark.edges;
+      let categories = [];
+      posts.forEach(({ node }) => {
         createPage({
           path: node.fields.slug,
-          component: path.resolve(`./src/templates/blog-post.js`),
+          component: blogPostTemplate,
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
             slug: node.fields.slug,
           },
-        })
-      })
-      resolve()
-    })
-  })
+        });
+        if (get(['frontmatter', 'category'])(node)) {
+          categories = categories.concat(node.frontmatter.category)
+        }
+      });
+      categories = uniq(categories);
+      categories.forEach((category) => {
+        createPage({
+          path: `/${kebabCase(category)}/`,
+          component: categoryTemplate,
+          context: {
+            category,
+          },
+        });
+      });
+      resolve();
+    });
+  });
 };
