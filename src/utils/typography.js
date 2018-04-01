d541d25ddff54ab35b908a496b3f1c777cede00f
Detik <the2ndflamingheart@gmail.com>
2018-04-02 04:03:03 +0700

    Set h3 typography margin bottom to 0 rythim

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,7 +2,7 @@ import Typography from 'typography';
 import noriegaTheme from 'typography-theme-noriega';
 
 noriegaTheme.overrideThemeStyles = ({ rhythm }) => ({
-  h2: {
+  'h2, h3': {
     marginBottom: rhythm(0),
   },
 });
