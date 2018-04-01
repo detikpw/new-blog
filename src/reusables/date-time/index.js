8816f3b41bef2908b346a75e1e8dda14797a3201
Detik <the2ndflamingheart@gmail.com>
2018-04-02 03:52:03 +0700

    Change datetime font color to black

--- src/reusables/date-time/index.js
+++ src/reusables/date-time/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Italic } from '../typography';
-import { GREY } from '../../constants/color';
 
 const Date = styled.time`
   display: block;
-  color: ${GREY};
+  color: 'black';
   line-height: 1.8em;
   margin-bottom: 1.3334em;
   font-weight: 1000;
